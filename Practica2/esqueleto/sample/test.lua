c28f6656815d6ba95107c4e3b708ccb60cc726b3
Lepticidio <miguel.andujar@live.u-tad.com>
2020-06-28 11:28:26 +0200

    Lua modified

--- Practica2/esqueleto/sample/test.lua
+++ Practica2/esqueleto/sample/test.lua
@@ -34,33 +34,35 @@ enemy:setProp(prop, layer)
 enemy:checkIsEnemy(true)
 -- Start the character (allow calls to OnUpdate)
 enemy:start()
-enemy:setLoc(-500, 0)
+enemy:setLoc(-400, 0)
 enemy:setRot(0)
 enemy:setLinearVel(100, 0)
 --entity:setAngularVel(30)
 
 --Texture of dragon character
-gfxQuad = createImage("dragon.png", 64)
-gfxQuad1 = createImage("dragonDead.png", 64)
-gfxQuad2 = createImage("dragonAttack.png", 64)
+gfxQuad = createImage("dragon_idle.png", 64)
+gfxQuad1 = createImage("dragon_alarm.png", 64)
+gfxQuad2 = createImage("dragon_windup.png", 64)
+gfxQuad3 = createImage("dragon_attack.png", 64)
 
 prop = MOAIProp2D.new()
 prop:setDeck(gfxQuad)
 entity  = Character.new()
 
 -- Add prop to be the renderable for this character
-entity:addImage(gfxQuad)    -- SetImage(0)  dragon.png
-entity:addImage(gfxQuad1)   -- SetImage(1)  dragonDead.png
-entity:addImage(gfxQuad2)   -- SetImage(2)  dragonAttack.png
+entity:addImage(gfxQuad)    -- SetImage(0)  idle
+entity:addImage(gfxQuad1)   -- SetImage(1)  alert
+entity:addImage(gfxQuad2)   -- SetImage(2)  windup
+entity:addImage(gfxQuad3)   -- SetImage(3)  attack
 
 -- Add prop to be the renderable for this character
 entity:setProp(prop, layer)
 entity:checkIsEnemy(false, enemy);
 -- Start the character (allow calls to OnUpdate)
 entity:start()
-entity:setLoc(0, 0)
+entity:setLoc(-200, 0)
 entity:setRot(0)
-entity:setLinearVel(100, 0)
+entity:setLinearVel(0, 0)
 --entity:setAngularVel(30)
 
 -- Enable Debug Draw
