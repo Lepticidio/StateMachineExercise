babadb93c0c0e24f7e9b4fdf7518b134329e5f42
Lepticidio <miguel.andujar@live.u-tad.com>
2020-06-28 11:15:01 +0200

    ActionPursue

---
+++ Practica2/esqueleto/ActionPursue.cpp
@@ -0,0 +1,22 @@
+#include "ActionPursue.h"
+#include "character.h"
+#include "PursueSteering.h"
+ActionPursue::ActionPursue(Character* _pOwner, Character* _pTarget) : m_pOwner(_pOwner)
+{
+	m_pPursueSteering = new PursueSteering(m_pOwner->GetArrive(), m_pOwner);
+	m_pPursueSteering->SetTarget(_pTarget);
+}
+void ActionPursue::start()
+{
+	m_pOwner->SetImage(1);
+}
+void ActionPursue::update()
+{
+	USVec2D vAcceleration = m_pPursueSteering->GetSteering();
+
+	//USVec2D vAcceleration = m_pPathSteering->GetSteering();
+	//USVec2D vAcceleration (0,0);
+	USVec2D vCurrentVelocity = m_pOwner->GetLinearVelocity() + vAcceleration * m_pOwner->GetLastStep();
+	m_pOwner->SetLinearVelocity(vCurrentVelocity.mX, vCurrentVelocity.mY);
+	m_pOwner->SetLoc(m_pOwner->GetLoc() + m_pOwner->GetLinearVelocity() * m_pOwner->GetLastStep());
+}
---
+++ Practica2/esqueleto/ActionPursue.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <stdafx.h>
+#include "Action.h"
+class Character;
+class PursueSteering;
+class ActionPursue : public Action
+{
+	Character* m_pOwner;
+	PursueSteering* m_pPursueSteering;
+public:
+	ActionPursue(Character* _pOwner, Character* _pTarget);
+	void start();
+	void update();
+};
+
